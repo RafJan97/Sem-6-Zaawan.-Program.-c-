6b79179dd923cfff2fe63a5834ec2e318686ef6e
RafJan97 <rafaljanas97@gmail.com>
2020-03-19 16:24:31 +0100

    Update main.cpp

--- LAB1/source/main.cpp
+++ LAB1/source/main.cpp
@@ -1,53 +1,333 @@
+// zadanie domowe 1 . Uzytkownik losuje liczbe, komputer zgaduje na podstawie odpowiedzi.
+
 #include <Windows.h>
 #include <cstdio>
-// otworzyles swe usta i wyrzekles me imie ³oooo panie
 #define IDLESS  IDNO
 #define IDMORE  IDYES
 #define IDHIT   IDCANCEL
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR szCmdLine, _In_ int iCmdShow)
 {
-  const wchar_t* csInfo = L"(Tak - wiêcej, Nie - mniej, Anuluj - trafione)\0";
-  wchar_t sText[256] = {};
+	{
+		int iRetKey = MessageBox(0, TEXT("Wybierz liczbe z zakresu 1-40"), TEXT("Aplikacja"), MB_OK);
 
-  int iMin = 0; //dolna granica mo¿liwego zakresu w którym le¿y liczba
-  int iMax = 40; //górna granica
-  int iOffset = 0; //przesuniêcie którejœ z granic po odpowiedzi u¿ytkownika
-  int iGuess = (iMax - iMin) / 2; //liczba "strza³"
+		int iRetKey1 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 20?"), TEXT("Aplikacja"), MB_YESNO);
+		if (iRetKey1 == IDYES)
+		{
+			int iRetKey2 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 30?"), TEXT("Aplikacja"), MB_YESNO);
+			if (iRetKey2 == IDYES)
+			{
+				int iRetKey3 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 35?"), TEXT("Aplikacja"), MB_YESNO);
+				if (iRetKey3 == IDYES)
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 38?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 39?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 40"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 39"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 37?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 38"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 36?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 37"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 36"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+				else
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 33?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 34?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 35"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 34"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 32?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 33"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 31?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 32"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 31"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+			}
+			// ************************ kolumna 25
+			else
+			{
+				int iRetKey3 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 25?"), TEXT("Aplikacja"), MB_YESNO);
+				if (iRetKey3 == IDYES)
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 28?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 29?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 30"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 29"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 27?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 28"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 26?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 27"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 26"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+				else
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 23?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 24?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 25"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 24"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 22?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 23"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 21?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 22"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 21"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+			}
+		}
+		// *******************************kolumna 10
+		else
+		{
+			int iRetKey2 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 10?"), TEXT("Aplikacja"), MB_YESNO);
+			if (iRetKey2 == IDYES)
+			{
+				int iRetKey3 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 15?"), TEXT("Aplikacja"), MB_YESNO);
+				if (iRetKey3 == IDYES)
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 18?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 19?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 20"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 19"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 17?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 18"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 16?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 17"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 16"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+				else
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 13?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 14?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 15"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 14"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 12?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 13"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 11?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 12"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 11"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+			}
+			// ************************ kolumna 5
+			else
+			{
+				int iRetKey3 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 5?"), TEXT("Aplikacja"), MB_YESNO);
+				if (iRetKey3 == IDYES)
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 8?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 9?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 10"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 9"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 7?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 8"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 6?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 7"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 6"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+				else
+				{
+					int iRetKey4 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 3?"), TEXT("Aplikacja"), MB_YESNO);
+					if (iRetKey4 == IDYES)
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 4?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 5"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							MessageBox(0, TEXT("Twoja liczba to 4"), TEXT("Aplikacja"), MB_OK);
+						}
+					}
+					else
+					{
+						int iRetKey5 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 2?"), TEXT("Aplikacja"), MB_YESNO);
+						if (iRetKey5 == IDYES)
+						{
+							MessageBox(0, TEXT("Twoja liczba to 3"), TEXT("Aplikacja"), MB_OK);
+						}
+						else
+						{
+							int iRetKey6 = MessageBox(0, TEXT("Czy ta liczba jest wieksza od 1?"), TEXT("Aplikacja"), MB_YESNO);
+							if (iRetKey6 == IDYES)
+							{
+								MessageBox(0, TEXT("Twoja liczba to 2"), TEXT("Aplikacja"), MB_OK);
+							}
+							else
+							{
+								MessageBox(0, TEXT("Twoja liczba to 1"), TEXT("Aplikacja"), MB_OK);
+							}
+						}
+					}
+				}
+			}
+		}
 
-  wsprintf(sText, L"Pomyœl liczbê od 0 do 40.\nPo podaniu propozycji, okreœl czy twoja wybrana liczba jest\nwiêksza lub mniejsza.\n%s", csInfo);
-    
-  int iRet = MessageBoxW(0, sText, L"Gierka", MB_OKCANCEL);
-  
-  if (iRet == IDCANCEL)
-    return 0;
-
-  do
-  {
-    wsprintf(sText, L"Twoja liczba to %d?\n%s", iGuess, csInfo);
-    iRet = MessageBoxW(0, sText, L"Strza³", MB_YESNOCANCEL);
-
-    if (iRet == IDLESS)
-      iMax = iGuess - 1;
-    else if (iRet == IDMORE)
-      iMin = iGuess + 1;
-
-    if (iMin == iMax)
-    {
-      iGuess = iMax;
-      break;
-    }
-    
-    iGuess = ((iMax - iMin) / 2 + iMin);
-  } while (iRet != IDHIT);
-
-  wsprintf(sText, L"Twoja liczba to %d", iGuess);
-  MessageBoxW(0, sText, L"Zgadniête", MB_OK);
-
-  return 0;
+		return 0;
+	}
 }
-/*
-zadanie - gra w "jak¹ liczbê mam na myœli"
-zakres 1-40, komputer zgaduje, u¿ytkownik wybiera
-drzewko binarne zgadywanka
-*/
